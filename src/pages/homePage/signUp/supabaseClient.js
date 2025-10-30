22d8e3fa0572f1933ab18590bbc9d30412416bf7
Akanxi Kushwaha <159679181+akanxikushwaha@users.noreply.github.com>
2025-10-30 16:53:26 +0530

    environment var used for key

--- src/pages/homePage/signUp/supabaseClient.js
+++ src/pages/homePage/signUp/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://fqjoohzcvsvlfmjcucmq.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxam9vaHpjdnN2bGZtamN1Y21xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzMTEzODIsImV4cCI6MjA3Mzg4NzM4Mn0.BjnGZLiXBRBTSO7uYFxsE4mqA5G_l_WVN5g1vsvMvvw"
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
